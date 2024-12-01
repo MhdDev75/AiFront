756d53393136df402524fec9bf6ef014d90573fa
Mahdi Ebrahimpour <149631776+MhdDev75@users.noreply.github.com>
2024-12-01 13:08:31 +0330

    add welcome page image

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,65 +1,78 @@
-"use client"
-import { useEffect, useState } from 'react';
-import { useRouter } from 'next/navigation';
-import { BotIcon } from 'lucide-react';
-import { useTranslations } from 'next-intl';
+"use client";
+import { useEffect, useState } from "react";
+import { useRouter } from "next/navigation";
+import { BotIcon } from "lucide-react";
+import { useTranslations } from "next-intl";
 
 const LandingPage: React.FC = () => {
-    const [progress, setProgress] = useState(0);
-    const router = useRouter();
-    const t = useTranslations("i18n")
+  const [progress, setProgress] = useState(0);
+  const router = useRouter();
+  const t = useTranslations("i18n");
 
-    useEffect(() => {
-        const totalDuration = 3000; // مدت زمان نمایش صفحه فرود در میلی‌ثانیه (اینجا 3 ثانیه)
-        const increment = 100; // هر چند میلی‌ثانیه یک بار پیشرفت نوار به‌روز شود
-        const steps = totalDuration / increment;
+  useEffect(() => {
+    const totalDuration = 3000; // مدت زمان نمایش صفحه فرود در میلی‌ثانیه (اینجا 3 ثانیه)
+    const increment = 100; // هر چند میلی‌ثانیه یک بار پیشرفت نوار به‌روز شود
+    const steps = totalDuration / increment;
 
-        let currentStep = 0;
-        const timer = setInterval(() => {
-            currentStep++;
-            setProgress((currentStep / steps) * 100);
+    let currentStep = 0;
+    const timer = setInterval(() => {
+      currentStep++;
+      setProgress((currentStep / steps) * 100);
 
-            if (currentStep >= steps) {
-                clearInterval(timer);
-                router.push('/welcome'); // هدایت به صفحه اصلی
-            }
-        }, increment);
+      if (currentStep >= steps) {
+        clearInterval(timer);
+        router.push("/welcome"); // هدایت به صفحه اصلی
+      }
+    }, increment);
 
-        return () => clearInterval(timer); // پاکسازی تایمر
-    }, [router]);
+    return () => clearInterval(timer); // پاکسازی تایمر
+  }, [router]);
 
-    return (
-        <div className=' bg-center bg-no-repeat bg-contain bg-[url("/images/bg-ai.avif")]' style={styles.container}>
-            <div className='flex flex-col justify-center gap-8 items-center text-center backdrop-blur-sm w-full'  style={styles.container}>
-            <div className='flex flex-col items-center gap-3'>
-                <BotIcon size={150} />
-                <h1 className='text-lg text-center'>{t("welcome")}</h1>
-            </div>
-
-            <div style={styles.progressBarContainer}>
-                <div className='bg-primary ' style={{ ...styles.progressBar, width: `${progress}%` }} />
-            </div>
-            </div>
+  return (
+    <div
+      className=' bg-center bg-no-repeat bg-cover bg-[url("/images/bg-ai.jpg")] bg-slate-900  shadow-slate-800'
+      style={styles.container}
+    >
+      <div
+        className="flex flex-col justify-center gap-2 items-center backdrop-blur-sm text-center  w-full"
+        style={styles.container}
+      >
+        <div className="flex flex-col items-center gap-3 ">
+          <BotIcon color="white" size={150} />
         </div>
-    );
+        <div className="flex flex-col items-center gap-3 ">
+          <span className="text-3xl font-extrabold text-white">The Ai Studio</span>
+        </div>
+        <div className="flex flex-col items-center gap-3 absolute bottom-24">
+          <h1 className="text-lg text-white font-bold text-center">
+            {t("welcome")}
+          </h1>
+          <div className="w-full" style={styles.progressBarContainer}>
+            <div
+              className="bg-primary "
+              style={{ ...styles.progressBar, width: `${progress}%` }}
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 const styles = {
-    container: {
-        height: '100vh',
-    },
-    progressBarContainer: {
-        width: '80%',
-        height: '60px',
-        backgroundColor: '#e0e0e0',
-        borderRadius: '5px',
-        overflow: 'hidden',
-        marginTop: '20px',
-    },
-    progressBar: {
-        height: '100%',
-        transition: 'width 0.1s ease-in-out',
-    },
+  container: {
+    height: "100vh",
+  },
+  progressBarContainer: {
+    height: "10px",
+    backgroundColor: "#e0e0e0",
+    borderRadius: "5px",
+    overflow: "hidden",
+  },
+  progressBar: {
+    height: "100%",
+    transition: "width 0.1s ease-in-out",
+  },
 };
 
 export default LandingPage;
--- src/app/welcome/page.tsx
+++ src/app/welcome/page.tsx
@@ -1,19 +1,93 @@
-"use client"
+"use client";
 
-import { Card, CardContent } from '@/components/ui/card';
+import slide1 from "@/assets/images/welcome/slide-1.gif";
+import slide2 from "@/assets/images/welcome/slide-2.gif";
+import slide3 from "@/assets/images/welcome/slide-3.gif";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  type CarouselApi,
+} from "@/components/ui/carousel";
+import Image from "next/image";
+import { useEffect, useState } from "react";
+import { Button } from "@/components/ui/button";
+// import { Icon } from "lucide-react";
 
+const WelcomePage = () => {
+  const [api, setApi] = useState<CarouselApi>();
+  const [current, setCurrent] = useState(0);
+  const [count, setCount] = useState(0);
 
-const WelcomePage: React.FC = () => {
-    return (
-        <div className="p-1">
-            <Card>
-                <CardContent className="flex aspect-square items-center justify-center p-6">
-                    <span className="text-4xl font-semibold">5</span>
-                </CardContent>
-            </Card>
-        </div>
+  useEffect(() => {
+    if (!api) {
+      return;
+    }
 
-    );
+    setCount(api.scrollSnapList().length);
+    setCurrent(api.selectedScrollSnap() + 1);
+
+    api.on("select", () => {
+      setCurrent(api.selectedScrollSnap() + 1);
+    });
+  }, [api]);
+
+  const welcome = [
+    {
+      id: 1,
+      title: "هوش مصنوعی",
+      description:
+        "استقاده از تمامی قابلیت های هوش مصنوعی متل چت جی پی تی و کانورتور های عکس و ویدیو و جزو کامل ترین پکیح ها",
+      url: slide1,
+    },
+    {
+      id: 2,
+      title: "سرعت بالا",
+      description:
+        "کیفیت حرف اول رو میزنه پس با ما همراه باشه تا بتونیم کیفیت رو به نمایش بزاریم ",
+      url: slide2,
+    },
+    {
+      id: 3,
+      title: "بخش رایگان",
+      description:
+        "برای اینکه بتونی حس خوبی داشته باشی یه سری از بخش ها به صورت رایگام میتونی استفاده کنی پس میتونی با زدن دکمه زیر با مه همراه باشی ",
+      url: slide3,
+    },
+  ];
+  return (
+    <div className="h-screen  flex items-center">
+      <Carousel dir="ltr" setApi={setApi}>
+        <CarouselContent className="-ml-2 md:-ml-4">
+          {welcome.map((item, index) => (
+            <CarouselItem className="h-full w-full p-0" key={item.id}>
+                <div className="flex flex-col gap-2 items-center justify-center ">
+                  <Image  src={item.url} alt={item.title} />
+                  <span className="text-3xl font-semibold text-black">
+                    {item.title}
+                  </span>
+                  <p className="text-md text-center p-6 text-gray-500 font-semibold">
+                    {item.description}
+                  </p>
+                  {welcome.length == index + 1 && (
+                    <Button size={"lg"} className=" rounded">
+                      بزن بریم
+                    </Button>
+                  )}
+                </div>
+            </CarouselItem>
+          ))}
+        </CarouselContent>
+      </Carousel>
+      <div className="py-2 w-full text-center absolute bottom-20 left-auto  text-sm text-muted-foreground">
+        صفحه {current} از {count}
+        {/* {Array(count).map((item)=>{
+            
+            <Icon className={current == item ? '':} />
+        })} */}
+      </div>
+    </div>
+  );
 };
 
 export default WelcomePage;
