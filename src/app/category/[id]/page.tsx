58838b959125c18b6d1049d285a151826df2c07e
MahdiDev75 <149631776+MhdDev75@users.noreply.github.com>
2024-12-03 23:16:50 +0330

    add category button

--- src/app/category/[id]/page.tsx
+++ src/app/category/[id]/page.tsx
@@ -6,16 +6,17 @@ import { useRouter } from "next/navigation";
 import { useBackButton } from "@/core/telegram/BackButtonProvider";
 import { ICategoryProps } from "@/lib/type";
 import seed from "@/seeds/mockdata.json"
+import { ArrowUpLeft, ArrowUpRight } from "lucide-react";
+import { useLocale } from "next-intl";
+import { locales } from "@/core/i18n/config";
+import Image from "next/image";
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 const CategoryPage = ({ params: { id } }: any) => {
-    console.log(id);
-
-
     const router = useRouter();
-
+    const locale = useLocale();
+    const isIr = locale == locales[0] ? true : false;
     console.log(router);
-
     // const t = useTranslations("i18n");
     const { setIsVisible } = useBackButton();
     const [data, setData] = useState<ICategoryProps | null | undefined>(null);
@@ -31,11 +32,21 @@ const CategoryPage = ({ params: { id } }: any) => {
     }, []);
 
     return (
-        <section className="px-5 h-full bg-gradient-to-b from-indigo-500 from-10% via-sky-500 via-30% to-emerald-500 to-90%">
+        <section className="px-5 h-full">
             {data?.items.title}
-            {data?.items.itemList.map((item: any) => (
-                <div key={item.id}>{item.title}</div>
-            ))}
+            <div className="grid grid-cols-2 md:grid-cols-4 lg:grid-cols-6">
+                {data?.items.itemList.map((item: any) => (
+                    <div key={item.id} className="bg-secondary flex flex-col gap-2 rounded-custom">
+                        <div className="flex flex-row justify-between">
+                            <div className="p-2 rounded-full bg-slate-800 bg-opacity-35">
+                                <Image src={item.imgUrl} width={120} height={120} className="rounded-custom" alt={item.title} />
+                            </div>
+                            {isIr ? <ArrowUpLeft size={20} /> : <ArrowUpRight size={20} />}
+                        </div>
+                        <span className="font-bold">{item.title}</span>
+                    </div>
+                ))}
+            </div>
         </section>
     );
 };
