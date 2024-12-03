d0578066c713c62d5cfe285ee2e6e52aad14ffab
MahdiDev75 <149631776+MhdDev75@users.noreply.github.com>
2024-12-04 00:33:00 +0330

    customize flowing

--- src/app/category/[id]/page.tsx
+++ src/app/category/[id]/page.tsx
@@ -6,7 +6,7 @@ import { useTranslations } from "next-intl";
 import { useBackButton } from "@/core/telegram/BackButtonProvider";
 import { ICategoryProps } from "@/lib/type";
 import seed from "@/seeds/mockData.json"
-import { ArrowUpLeft, ArrowUpRight, StarIcon } from "lucide-react";
+import { ArrowUpLeft, ArrowUpRight, BadgePlus, StarIcon } from "lucide-react";
 import { useLocale } from "next-intl";
 import { locales } from "@/core/i18n/config";
 import Image from "next/image";
@@ -39,19 +39,24 @@ const CategoryPage = ({ params: { id } }: any) => {
                     <div key={item.id} className="bg-gray-600 bg-opacity-70 flex flex-col gap-2 rounded-custom p-3">
                         <div className="flex flex-row justify-between">
                             <Image src={item.imgUrl} width={40} height={40} className="rounded-custom" alt={item.title} />
-                            <div className="flex flex-row flex-nowrap items-center gap-2">
-                            <div className="bg-gray-500 bg-opacity-70 rounded-custom flex justify-center items-center">
-                                <StarIcon color="yellow" size={14} />
+                            <div className="flex flex-col flex-nowrap items-center gap-2">
+                                <div className="bg-gray-500 bg-opacity-70 rounded-custom flex flex-row flex-nowrap P-1 justify-center items-center">
+                                    <StarIcon color="yellow" size={12} />
+                                    <span className="font-bold text-xs">4.9</span>
+                                </div>
+                                <div className="bg-gray-500 bg-opacity-70 rounded-custom flex flex-row flex-nowrap P-1  justify-center items-center">
+                                    <BadgePlus color="yellow" size={12} />
+                                    <span className="font-bold text-xs">{(10010).toLocaleString()}</span>
+                                </div>
+
+
                             </div>
-                            <span className="font-bold text-sm">4.9</span>
-                            <span className="text-xs flex flex-col gap-1 text-gray-300">{(10010).toLocaleString()} <span>Reviews</span></span>
-                        </div>
                             {isIr ? <ArrowUpLeft size={20} /> : <ArrowUpRight size={20} />}
                         </div>
                         <span className="font-bold text-foreground">
                             {item.title.substring(0, 12)} {item.title.length > 12 && '...'}
                         </span>
-                       
+
                         <p className="text-sm text-primary-foreground text-pretty">
                             {item.description.substring(0, 50)} {item.description.length > 50 && '...'}
                         </p>
