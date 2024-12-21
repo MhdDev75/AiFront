5df0f3ab02b4e62b1c5da53d652478e03d63a0d3
Mahdi Ebrahimpour <149631776+MhdDev75@users.noreply.github.com>
2024-12-21 16:28:34 +0330

    remove env

--- api/apiClient.ts
+++ api/apiClient.ts
@@ -3,7 +3,7 @@ import axios from 'axios';
 import { toast } from 'react-toastify';
 
 const apiClient = axios.create({
-  baseURL: process.env.BASE_URL || 'http://64.44.167.150:7001/', // آدرس پایه API
+  baseURL: 'http://64.44.167.150:7001/', // آدرس پایه API
   timeout: 20000, // زمان انتظار درخواست‌ها
   headers: {
     'Content-Type': 'application/json',
