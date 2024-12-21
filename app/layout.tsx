f441e044d077e35533bed1adc216359f02b17fd6
Mahdi Ebrahimpour <149631776+MhdDev75@users.noreply.github.com>
2024-12-21 14:34:28 +0330

    no referrer

--- app/layout.tsx
+++ app/layout.tsx
@@ -39,7 +39,7 @@ export default async function RootLayout({
 
   return (
     <html lang={locale} data-theme={theme?.value ? theme?.value : 'dark'} className="h-full">
-      <head><meta name="referrer" content="origin" /></head>
+      <head><meta name="referrer" content="no-referrer" /></head>
       <body
         dir={locale == locales[0] ? "rtl" : "ltr"}
         className={`${locale == locales[0] ? iranSans.className : geistSans.variable
