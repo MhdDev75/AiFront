6f1a7fd0f1fd9b85c9a3c2db1120a66b7555980e
Mahdi Ebrahimpour <149631776+MhdDev75@users.noreply.github.com>
2024-12-21 18:23:21 +0330

    fixed reload page

--- app/reload/page.tsx
+++ app/reload/page.tsx
@@ -1,17 +1,21 @@
-"use client"
-import React from 'react'
-import { useRouter } from 'next/navigation'
+"use client";
+import React from "react";
+import { useRouter } from "next/navigation";
 
 const ReloadPage = () => {
-    const router = useRouter()
-    return (
-        <div className="flex flex-col gap-5   h-full justify-center items-center">
-            <h1 className='text-2xl'>اطلاعات شما بازیابی نشد </h1>
-            <button onClick={() => router.push('/')} className='btn btn-primary'>
-                بارگزاری مجدد
-            </button>
+  const router = useRouter();
+  return (
+    <div className="flex flex-col h-screen main-div">
+      <main className="container  mx-auto relative flex-1 overflow-y-auto px-4">
+        <div className="flex flex-col gap-3   overflow-hidden h-full justify-center items-center">
+          <h1 className="text-2xl">اطلاعات شما بازیابی نشد </h1>
+          <button onClick={() => router.push("/")} className="btn btn-primary">
+            بارگزاری مجدد
+          </button>
         </div>
-    )
-}
+      </main>
+    </div>
+  );
+};
 
-export default ReloadPage
+export default ReloadPage;
