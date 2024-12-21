172f3fb908ccfb3ef4ea9167c340e4a715567346
MahdiDev75 <149631776+MhdDev75@users.noreply.github.com>
2024-12-21 22:15:10 +0330

    Disconected Error

--- app/reload/page.tsx
+++ app/reload/page.tsx
@@ -1,16 +1,18 @@
 "use client";
 import React from "react";
 import { useRouter } from "next/navigation";
+import { useTranslations } from "next-intl";
 
 const ReloadPage = () => {
+  const t= useTranslations("i18n");
   const router = useRouter();
   return (
     <div className="flex flex-col h-screen main-div">
       <main className="container  mx-auto relative flex-1 overflow-y-auto px-4">
         <div className="flex flex-col gap-3   overflow-hidden h-full justify-center items-center">
-          <h1 className="text-2xl">اطلاعات شما بازیابی نشد </h1>
+          <h1 className="text-2xl">{t("ReloadError")}</h1>
           <button onClick={() => router.push("/")} className="btn btn-primary">
-            بارگزاری مجدد
+            {t("Reload")}
           </button>
         </div>
       </main>
