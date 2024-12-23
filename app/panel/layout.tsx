1bfe9e404be7778bdb7d21269443c8bb389a57f0
MahdiDev75 <149631776+MhdDev75@users.noreply.github.com>
2024-12-24 02:42:22 +0330

    refactor PanelLayout component to increase maximum width of layout container

--- app/panel/layout.tsx
+++ app/panel/layout.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 
 const PanelLayout: React.FC<{ children: React.ReactNode }> = ({ children }) => {
   return (
-    <div className="flex flex-col max-w-md  h-screen main-div pt-20">
+    <div className="flex flex-col max-w-2xl  h-screen main-div pt-20">
       <main className="container  mx-auto relative flex-1 overflow-y-auto px-4 py-2">{children}</main>
       <Footer />
     </div>
